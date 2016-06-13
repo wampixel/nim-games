ef26639e84a19112eb11f7378ed0fa3c7a9229ed
wampixel <wampixel@gmail.com>
2016-06-13 14:29:35 +0200

    ajout de commentaires pour le jeu de nim simple

--- python_sources/simple-nim-game/ia.py
+++ python_sources/simple-nim-game/ia.py
@@ -1,7 +1,13 @@
-#!/usr/bin/python3
 
 from random import randrange
 
+"""
+    calcul le coup gagnant d'un jeu de Nim simple (jeu des tas)
+    fait la somme de Nim de toutes les valeurs des tas et en déduit si il existe un coup gagnant
+
+    si jamais le coup gagnant n'existe pas, retourne le tuple (0, 1) pour enlever dans le premier tas 1 alumette
+    sinon retourne le coup gagnant
+"""
 def getwinningStrat(l) :
     s = 0
     for i in l:
@@ -24,6 +30,9 @@ def getwinningStrat(l) :
                         k += 1
                 i += 1
 
+"""
+    calcul et retourne la somme de Nim du jeu donné en entrée
+"""
 def num_add(l) :
     i = 0
     s = 0
@@ -32,6 +41,12 @@ def num_add(l) :
         i += 1
     return s
 
+"""
+    retourne aléatoirement le coup a jouer
+    cette fonction permet de ne suivre aucune méthode pour connaitre le coup gagnant
+    si elle réussi a gagner, soit elle a trouvé aléatoirement le coup gagnant, soit le joueur n'as pas reussi a trouver 
+    de stratégie gagnante.
+"""
 def randomStrat(l) :
     tas = randrange(len(l))
     k = randrange(1, l[tas] + 1)
